aba9d976d8ef371f15f4e454bf72e911145bfb72
Paul Kibby <kibby.ru@yandex.ru>
2018-05-18 22:43:01 +0400

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,5 @@
 <?php
 
-ini_set('error_reporting', E_ALL);
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-
 require 'classes/Car.php';
 
 $car1 = new Car('Skoda', 'красный', 4);
